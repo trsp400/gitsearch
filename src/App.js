ed112fa0cd175c3c5ca8050fb2088c2f176b17de
Thiago Robles <roblesthiago38@gmail.com>
2020-08-09 15:47:21 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { StatusBar} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => (
+  <NavigationContainer>
+
+  </NavigationContainer>
+);
+
+export default App;
